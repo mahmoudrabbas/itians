e7c58b90b125eae68e2133ff96c91a3fc0f3b702
mokhtar <moktarmoha17@gmail.com>
2026-01-20 17:34:54 +0200

    add username validation

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,26 @@
 // write login validation here
 
 
-
+alert("s")
 
 // and write signup validation here
+function signup(){
+
+ let isvalid=true
+ let username=document.getElementById("username")  
+ let emai=document.getElementById("email")  
+ let password=document.getElementById("password")  
+ let confirmpassword=document.getElementById("confirmpassword")  
+
+ let name=username.value.trim()
+let nameregex=/^[A-Za-z]+$/
+
+if(name.length<3)
+ {   isvalid=false;
+showError(username, "Username must be at least 3 characters");
+}
+else if(!nameregex.test(name))
+    isvalid=false
+
+
+}
